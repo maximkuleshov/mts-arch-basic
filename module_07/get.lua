5a86df6497c79018aff22487f03dd3dc2bf40d3b
mkulesho <maksim.kuleshov@t-systems.com>
2023-05-17 00:53:17 +0300

    Implemented homework 06 for module 07

--- module_07/get.lua
+++ module_07/get.lua
@@ -2,10 +2,16 @@ local frandom = io.open("/dev/urandom", "rb")
 local d = frandom:read(4)
 math.randomseed(d:byte(1) + (d:byte(2) * 256) + (d:byte(3) * 65536) + (d:byte(4) * 4294967296))
 
-number =  math.random(1,100000)
+local file = io.open("oids.txt", "r");
+local oids = {}
+for line in file:lines() do
+    table.insert (oids, line);
+end
+
 request = function()
     headers = {}
+    number =  math.random(1,100000)
     headers["Content-Type"] = "application/json"
     body = ''
-    return wrk.format("GET", "/authors/".. tostring(number), headers, body)
+    return wrk.format("GET", "/authors/".. oids[number], headers, body)
 end
