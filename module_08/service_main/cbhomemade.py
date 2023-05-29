65a9dbf2e50a1d3a154efe35be8030928e99630b
mekuleshov <mekuleshov@mts.ru>
2023-05-29 19:04:44 +0300

    Fixed custom CB half-open reporting

--- module_08/service_main/cbhomemade.py
+++ module_08/service_main/cbhomemade.py
@@ -11,8 +11,15 @@ class Status(Enum):
 class CircuitBreakerHome:
     def __init__(self, n):
         self.name = n
-        self.status = Status.CLOSED
+        self._status = Status.CLOSED
         self.failures = 0
+
+    @property
+    def status(self):
+        if (self._status == Status.OPEN) and (self.latest_fail_ts + timedelta(seconds = self.timeout) < datetime.now()):
+            return Status.HALF_OPEN
+        else:
+            return self._status
     
     def wrap(self, func, exception, attempts, timeout, fallback):
         self.func = func
@@ -25,16 +32,14 @@ class CircuitBreakerHome:
 
     def wrapper(self, *args, **kwargs):
         if self.status == Status.OPEN:
-            if self.latest_fail_ts + timedelta(seconds = self.timeout) > datetime.now():
-                return self.fallback(*args, **kwargs)
+            return self.fallback(*args, **kwargs)
+        if self.status == Status.HALF_OPEN:
             print("Timeout elapsed, retrying...")
-            self.status = Status.HALF_OPEN
-
         try:
             result = self.func(*args, **kwargs)
             if self.status == Status.HALF_OPEN:
                 print("Ok. Going back to CLOSED")
-                self.status = Status.CLOSED
+                self._status = Status.CLOSED
                 self.failures = 0
             return result
         except Exception as err:
@@ -46,8 +51,10 @@ class CircuitBreakerHome:
         self.latest_fail_ts = datetime.now()
         self.failures += 1
         if self.failures >= self.attempts:
-            print(f"Failed attempt count: {self.failures}, switching to OPEN")
-            self.status = Status.OPEN
+            print(f"Failed attempt count: {self.failures}")
+            if self.status != Status.OPEN:
+                print("Switching to OPEN")
+                self._status = Status.OPEN
 
 
     
